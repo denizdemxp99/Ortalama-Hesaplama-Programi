1056e18edbb2f49cd41e5baa9168aa0a359edc1e
Mustafa Deniz Demirkıran <96746079+denizdemxp99@users.noreply.github.com>
2022-05-05 12:08:04 +0300

    Proje Dosyası
    Basit C++ Örnekleri.

---
+++ ana.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int not1,not2;
+	
+	cout<<"1.Donem Notunu Giriniz:";
+	cin>>not1;
+
+	cout<<"2.Donem Notunu Giriniz:";
+	cin>>not2;
+	
+	cout<<not1+not2;
+	
+	
+    printf("Sinifi Gecti \n");
+    printf("Sinifta Kaldi\n");
+	
+	return 0;
+    
+}
