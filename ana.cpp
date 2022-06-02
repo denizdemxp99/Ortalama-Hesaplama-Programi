19bf89c90f7b2521a4ca4a0609fae8098db4bf8f
Mustafa Deniz Demirkıran <96746079+denizdemxp99@users.noreply.github.com>
2022-06-02 22:24:59 +0300

    Hatalar Düzeltildi.
    Bana Yardımcı Olan PrismaB'Ye Teşekkür Ederim.

--- ana.cpp
+++ ana.cpp
@@ -2,20 +2,23 @@
 using namespace std;
 int main()
 {
-	int not1,not2;
-	
-	cout<<"1.Donem Notunu Giriniz:";
-	cin>>not1;
-
-	cout<<"2.Donem Notunu Giriniz:";
-	cin>>not2;
-	
-	cout<<not1+not2;
-	
-	
-    printf("Sinifi Gecti \n");
-    printf("Sinifta Kaldi\n");
-	
-	return 0;
+    int not1,not2;
     
+    cout << ("1.Donem Notunu Giriniz:");
+    cin>>not1;
+
+    cout << ("2.Donem Notunu Giriniz:");
+    cin >> not2;
+    int full = not1 + not2;
+    int total = full / 2;
+    if (total > 50)
+    {
+		cout << "sinıfi gectin ";
+	}
+	else{
+		cout << "kaldin ";
+	}
+    return 0;
 }
+
+// Düzenleme By Prisma B : https://github.com/prismaB
